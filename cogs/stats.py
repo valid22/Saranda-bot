f43bced3b48b5ba8d8fdd01ab5509797be12e186
Dote <10195318+valid22@users.noreply.github.com>
2021-04-25 21:39:20 +0530

    stats color

--- cogs/stats.py
+++ cogs/stats.py
@@ -19,7 +19,7 @@ class ServerStats(commands.Cog):
         role_member = discord.utils.get(ctx.guild.roles, name="Member")
         role_unverified = discord.utils.get(ctx.guild.roles, name="unverified")
         
-        embed=discord.Embed(title="Member Count", description="Count of members in this server", color="0x2f92d0")
+        embed=discord.Embed(title="Member Count", description="Count of members in this server", color=0x2f92d0)
         embed.set_author(name="Saranda House Stats")
         embed.add_field(name="Total Users", value=ctx.guild.member_count, inline=True)
         embed.add_field(name="Members", value=role_member.members.size, inline=True)
